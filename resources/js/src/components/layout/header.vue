55d93f7fa00ee2bfaad00230994abb29d129d495
Eustacio <arellanos.baaeus@gmail.com>
2022-05-03 16:54:34 -0500

    Modificacion header

--- resources/js/src/components/layout/header.vue
+++ resources/js/src/components/layout/header.vue
@@ -10,7 +10,7 @@
                         </router-link>
                     </li>
                     <li class="nav-item theme-text">
-                        <router-link to="/" class="nav-link"> CYP </router-link>
+                        <router-link to="/" class="nav-link"> CYP dev</router-link>
                     </li>
                 </ul>
                 <div class="d-none horizontal-menu">
