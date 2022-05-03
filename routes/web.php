e19ead4c218e92a62294d6f88d675429fde9d613
Eustacio <arellanos.baaeus@gmail.com>
2022-05-03 12:39:54 -0500

    Update Laravel

--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-<<<<<<< HEAD
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
-
-/*Route::group(['middleware'=> 'web'], function(){
-    
-});*/
-
-Route::get('/', 'App\Http\Controllers\UserController@getLogin')->name('login');
-Route::get('/dashboard', 'App\Http\Controllers\UserController@dashboard')->name('dashboard');
-Route::get('/register', 'App\Http\Controllers\UserController@getRegister')->name('register-get');
-Route::get('/user/{id}/edit', 'App\Http\Controllers\UserController@getEditUser')->name('Edit');
-Route::post('/register', 'App\Http\Controllers\UserController@postRegister')->name('register-post');
-Route::post('/login', 'App\Http\Controllers\UserController@postLogin')->name('login-post');
-Route::post('/user/{id}', 'App\Http\Controllers\UserController@PostEditUser')->name('edit-post');
-Route::post('/user/{id}/delete', 'App\Http\Controllers\UserController@PostDeleteUser')->name('Delete');
-Route::get('/logout', 'App\Http\Controllers\UserController@getlogout')->name('Logout-get');
-=======
 Route::get('/{any}', [AppController::class, 'index'])->where('any', '.*');
->>>>>>> dev
